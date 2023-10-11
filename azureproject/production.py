aeb9cdf38bdabb0bf10b11c1b4b9082e81c4baeb
kanesato <40815583+kanesato@users.noreply.github.com>
2023-10-11 16:49:15 +0900

    commit

--- azureproject/production.py
+++ azureproject/production.py
@@ -6,8 +6,8 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
+    dbuser=conn_str_params['ovdesqhray'],
+    dbpass=conn_str_params['M3Q70C28YL538FMK$'],
+    dbhost=conn_str_params['poc-appservice-01-server.postgres.database.azure.com'],
+    dbname=conn_str_params['poc-appservice-01-database']
 )
